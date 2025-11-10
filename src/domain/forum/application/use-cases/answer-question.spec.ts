863010eac63a014a276ba3e951e9e41798674034
Camila <camilinhaalmeida55@gmail.com>
2025-11-10 12:32:43 -0300

    ♻️ Refactoring the folders

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,5 +1,5 @@
 import { AnswerQuestionUseCase } from './answer-question'
-import type { Answer } from '../entities/answer'
+import type { Answer } from '../../enterprise/entities/answer'
 import type { AnswersRepository } from '../repositories/answers-repository'
 
 const fakeAnswersRepository: AnswersRepository = {
