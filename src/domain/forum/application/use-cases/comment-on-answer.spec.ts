57b9effd470e3426621772204d6e4c0abeb4ad25
Camila <camilinhaalmeida55@gmail.com>
2025-11-29 12:03:14 -0300

    ✅ Add Use case: Comment on the reply

---
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,36 @@
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answer-repository'
+import { makeAnswer } from 'test/factories/make-answer'
+import { CommentOnAnswerUseCase } from './comment-on-answer'
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryAnswerCommentsRespository: InMemoryAnswerCommentsRepository
+let sut: CommentOnAnswerUseCase
+
+describe('Comment on Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    inMemoryAnswerCommentsRespository = new InMemoryAnswerCommentsRepository()
+
+    sut = new CommentOnAnswerUseCase(
+      inMemoryAnswersRepository,
+      inMemoryAnswerCommentsRespository,
+    )
+  })
+
+  it('should be able to comment on answer.', async () => {
+    const answer = makeAnswer()
+
+    await inMemoryAnswersRepository.create(answer)
+
+    await sut.execute({
+      answerId: answer.id.toString(),
+      authorId: answer.authorId.toString(),
+      content: 'Comentário teste',
+    })
+
+    expect(inMemoryAnswerCommentsRespository.items[0]?.content).toEqual(
+      'Comentário teste',
+    )
+  })
+})
