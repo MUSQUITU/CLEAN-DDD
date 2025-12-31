5016eaf445605f56e1413d0ee60cf3657a1c7755
Camila <camilinhaalmeida55@gmail.com>
2025-12-31 19:55:29 -0300

    🚨 Incomplete attachments in the responses.

---
+++ src/domain/forum/application/repositories/answer-attachments-repository.ts
@@ -0,0 +1,6 @@
+import type { AnswerAttachment } from '../../enterprise/entities/answer-attachment'
+
+export interface AnswerAttachmentsRepository {
+  findManyByAnswerId(answerId: string): Promise<AnswerAttachment[]>
+  deleteManyByAnswerId(answer: string): Promise<void>
+}
--- src/domain/forum/enterprise/entities/answer.ts
+++ src/domain/forum/enterprise/entities/answer.ts
@@ -1,10 +1,12 @@
 import { Entity } from '@/core/entities/entity'
 import type { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import type { Optional } from '@/core/types/optional'
+import { AnswerAttachmentList } from './answer-attachment-list'
 
 export interface AnswerProps {
   authorId: UniqueEntityID
   questionId: UniqueEntityID
+  attachments: AnswerAttachmentList
   content: string
   createdAt: Date
   updatedAt?: Date
@@ -17,6 +19,10 @@ export class Answer extends Entity<AnswerProps> {
 
   get questionId() {
     return this.props.questionId
+  }
+
+  get attachments() {
+    return this.props.attachments
   }
 
   get content() {
@@ -44,13 +50,19 @@ export class Answer extends Entity<AnswerProps> {
     this.touch()
   }
 
+  set attachments(attachments: AnswerAttachmentList) {
+    this.props.attachments = attachments
+    this.touch()
+  }
+
   static create(
-    props: Optional<AnswerProps, 'createdAt'>,
+    props: Optional<AnswerProps, 'createdAt' | 'attachments'>,
     id?: UniqueEntityID,
   ) {
     const answer = new Answer(
       {
         ...props,
+        attachments: props.attachments ?? new AnswerAttachmentList(),
         createdAt: props.createdAt ?? new Date(),
       },
       id,
