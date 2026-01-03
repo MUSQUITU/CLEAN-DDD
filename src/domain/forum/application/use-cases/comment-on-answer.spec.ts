08a47b49e60cd14dc39811455396a82fc228920b
Camila <camilinhaalmeida55@gmail.com>
2026-01-03 18:14:42 -0300

    🐛 Corrections

--- src/domain/forum/application/use-cases/comment-on-answer.spec.ts
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -2,14 +2,20 @@ import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answer-re
 import { makeAnswer } from 'test/factories/make-answer'
 import { CommentOnAnswerUseCase } from './comment-on-answer'
 import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository'
+import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachments-repository'
 
+let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
 let inMemoryAnswersRepository: InMemoryAnswersRepository
 let inMemoryAnswerCommentsRespository: InMemoryAnswerCommentsRepository
 let sut: CommentOnAnswerUseCase
 
 describe('Comment on Answer', () => {
   beforeEach(() => {
-    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    inMemoryAnswerAttachmentsRepository =
+      new InMemoryAnswerAttachmentsRepository()
+    inMemoryAnswersRepository = new InMemoryAnswersRepository(
+      inMemoryAnswerAttachmentsRepository,
+    )
     inMemoryAnswerCommentsRespository = new InMemoryAnswerCommentsRepository()
 
     sut = new CommentOnAnswerUseCase(
